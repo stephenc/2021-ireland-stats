2a64feffa00d88421669b1ccff46dae153d52499
Stephen Connolly <stephen.alan.connolly@gmail.com>
2022-02-02 13:10:31 +0000

    Less verbose logging

--- scrape.sh
+++ scrape.sh
@@ -11,7 +11,7 @@ function curl_etag() {
     --etag-compare "$1.etag" \
     --remote-time \
     -o "$1.csv" \
-    $2 && echo ""
+    "$2" && echo ""
 }
 
 curl_etag \
@@ -52,12 +52,13 @@ curl_etag \
 # backing arcgis service behind geohive to get it
 # etags seem to be supported for conditional gets so shouldn't cause trouble
 # if we scrape it once an hour with a conditional get
+echo "Fetching COVID-19_Antigen.csv " 
 curl \
   -H 'Accept: application/json' \
   --etag-save "data/COVID-19_Antigen.etag" \
   --etag-compare "data/COVID-19_Antigen.etag" \
   --remote-time \
-  --output "data/COVID-19_Antigen.json" -v \
+  --output "data/COVID-19_Antigen.json" \
   'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/ArcGIS/rest/services/Registered_Positive_Antigen_View/FeatureServer/0/query?where=RegisteredPositiveAntigenFigure+is+not+null&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=DateOfData%2CRegisteredPositiveAntigenFigure&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=none&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=false&quantizationParameters=&sqlFormat=none&f=json&token='
 jq -r  '["DateOfData","RegisteredPositiveAntigenFigure"],(.features[].attributes | [(.DateOfData / 1000 | strftime("%Y/%m/%d %H:%M:%S+00")), .RegisteredPositiveAntigenFigure]) | @csv' data/COVID-19_Antigen.json > data/COVID-19_Antigen.csv
-
+echo ""
