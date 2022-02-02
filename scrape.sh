378d3ed7ec2597fb7cdaab8308663b840516a92c
Stephen Connolly <stephen.alan.connolly@gmail.com>
2022-02-02 09:24:01 +0000

    Found the antigen data source

--- scrape.sh
+++ scrape.sh
@@ -48,3 +48,16 @@ curl_etag \
   data/COVID-19_HPSC_HIU_Timeseries_Local_Electoral_Area_Mapped \
   https://covid-19.geohive.ie/datasets/7a10c7d87a634e71a1655e7451522b53_0.csv
 
+# This was the only source of antigen data I could find and had to go to the
+# backing arcgis service behind geohive to get it
+# etags seem to be supported for conditional gets so shouldn't cause trouble
+# if we scrape it once an hour with a conditional get
+curl \
+  -H 'Accept: application/json' \
+  --etag-save "data/COVID-19_Antigen.etag" \
+  --etag-compare "data/COVID-19_Antigen.etag" \
+  --remote-time \
+  --output "data/COVID-19_Antigen.json" -v \
+  'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/ArcGIS/rest/services/Registered_Positive_Antigen_View/FeatureServer/0/query?where=RegisteredPositiveAntigenFigure+is+not+null&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=DateOfData%2CRegisteredPositiveAntigenFigure&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=none&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=false&quantizationParameters=&sqlFormat=none&f=json&token='
+jq -r  '["DateOfData","RegisteredPositiveAntigenFigure"],(.features[].attributes | [(.DateOfData / 1000 | strftime("%Y/%m/%d %H:%M:%S+00")), .RegisteredPositiveAntigenFigure]) | @csv' data/COVID-19_Antigen.json > data/COVID-19_Antigen.csv
+
